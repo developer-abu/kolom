d47d97cc0b07f691c6a6aee061e5198c2ab2a911
developer-abu <officialworkofhurairashaikh01@gmail.com>
2026-09-02 17:51:32 +0530

    otp

--- server/config/mailer.js
+++ server/config/mailer.js
@@ -1,17 +1,32 @@
 require('dotenv').config({ path: '../.env' })
+const dns = require('dns');
 
 const nodemailer = require('nodemailer')
 
+const lookup = (hostname, options, callback) => {
+    dns.lookup(hostname, { family: 4 }, callback);
+};
 
 const transporter  = nodemailer.createTransport({
     host: "smtp.gmail.com",
     port: 587,
     secure: false,
-    family: 4,
+
+     connectionTimeout: 10000,
+    greetingTimeout: 10000,
+    socketTimeout: 10000,
+lookup:lookup,
     auth:{
         user:process.env.EMAIL_USER,
         pass:process.env.EMAIL_PASS
     }
 })
+transporter.verify((error, success) => {
+    if (error) {
+        console.log("SMTP VERIFY ERROR:", error);
+    } else {
+        console.log("SMTP SERVER READY:", success);
+    }
+});
 
 module.exports = transporter 
